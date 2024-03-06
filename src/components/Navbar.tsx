a349f568fcc77f93989422a1b15514a7113b6090
Marcin Dubinski <marcin.dubinski@bbraun.com>
2024-03-06 23:00:22 +0100

    add navbar and reasize it for mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
+
   return (
-    <div className='flex text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white border-b-4 border-gray-900'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Marcin De.</h1>
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>About</li>
             <li className='p-4'>Projects</li>
             <li className='p-4'>Contact</li>
         </ul>
+        <div onClick={handleNav} className='text-3xl cursor-pointer block md:hidden'>
+           { nav ? <AiOutlineMenu  /> : <AiOutlineClose  /> }
+        </div>
+        <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300] h-full ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Marcin De.</h1>
+        <ul className='pt-24 uppercase p-4'>
+            <li className='p-4 border-b border-gray-600'>Home</li>
+            <li className='p-4 border-b border-gray-600'>About</li>
+            <li className='p-4 border-b border-gray-600'>Projects</li>
+            <li className='p-4 border-b border-gray-600'>Contact</li>
+        </ul>
+        </div>
     </div>
   )
 }
